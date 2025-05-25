9873b69c41ec852c63dcd5a5f14d57e03416acce
Ihor Panasenko <ihor.panasenko14@gmail.com>
2025-05-25 20:26:57 +0300

    Add profile tab

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,26 +1,22 @@
-import Button from '@/components/Button'
 import ScreenWrapper from '@/components/ScreenWrapper'
 import Typo from '@/components/Typo'
-import { auth } from '@/config/firebase'
-import { colors } from '@/contansts/theme'
 import { useAuth } from '@/context/authContext'
-import { signOut } from 'firebase/auth'
 import React from 'react'
 import { StyleSheet } from 'react-native'
 
 const Home = () => {
     const {user} = useAuth();
-    console.log("User:", user);
-    const handleLogout = async () => {
-        await signOut(auth);
-    }
+    // console.log("User:", user);
+    // const handleLogout = async () => {
+    //     await signOut(auth);
+    // }
 
   return (
     <ScreenWrapper>
       <Typo>Home</Typo>
-      <Button onPress={handleLogout}>
+      {/* <Button onPress={handleLogout}>
         <Typo color={colors.black}>Logout</Typo>
-      </Button>
+      </Button> */}
     </ScreenWrapper>
   )
 }
